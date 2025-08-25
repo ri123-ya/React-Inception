0f2a417e1821a72f7211dc6d4936c52f854aeb0a
Riya Rastogi <riya02rastogi@gmail.com>
2025-08-26 05:07:48 +0530

    W can have named export and default export in the same file

---
+++ src/utils/constant.js
@@ -0,0 +1,5 @@
+export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" ;
+
+const LOGO_URL = "https://www.designmantic.com/logo-images/171406.png?company=Company%20Name&keyword=food&slogan=&verify=1";
+
+export default LOGO_URL;
