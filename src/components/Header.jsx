06983ade51b8923000a49ae53ae2c0198adc2687
Riya Rastogi <riya02rastogi@gmail.com>
2025-08-28 08:17:49 +0530

    Login/Logout button rendering

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 import  LOGO_URL  from "../utils/constant";
+import { useState } from "react";
 
 const Header = () => {
+  const [loginBtn, setLoginBtn] = useState("Login");
   return (
     <div className="header">
       <img
@@ -15,6 +17,15 @@ const Header = () => {
           <li>Contact Us</li>
           <li>About Us</li>
           <li>Cart</li>
+          <button className="login-btn"
+            onClick={() => {
+             loginBtn === "Login"
+              ? setLoginBtn("LogOut") 
+              :  setLoginBtn("Login");
+            }}
+          >
+            {loginBtn}
+          </button>
         </ul>
       </div>
     </div>
