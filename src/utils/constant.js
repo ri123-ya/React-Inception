21cdda346f96c120bedccda8ce00bef610eb356f
Riya Rastogi <riya02rastogi@gmail.com>
2025-09-03 06:42:14 +0530

    adding resturant url

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,5 +1,8 @@
 export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" ;
 
+export const MENU_URL = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=23.25050&lng=77.40650&restaurantId=";
+
+
 const LOGO_URL = "https://www.designmantic.com/logo-images/171406.png?company=Company%20Name&keyword=food&slogan=&verify=1";
 
 export default LOGO_URL;
