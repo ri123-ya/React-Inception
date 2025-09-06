5ec57ace1b64c84ccb9e46094ab6bb2c20c6d75c
Riya Rastogi <riya02rastogi@gmail.com>
2025-09-06 17:55:47 +0530

    Bundling the App using Lazy Loading and Suspense

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
@@ -8,12 +8,15 @@ import ResturantCard from "./components/ResturantCard";
 import Error from "./components/Error";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import ResturantMenu from "./components/ResturantMenu";
+// import Grocery from "./components/Grocery";
+
+const Grocery = lazy(() => import("./components/Grocery"));
 
 const AppLayout = () => {
   return (
     <div className="app">
       <Header />
-      <Outlet/>
+      <Outlet />
     </div>
   );
 };
@@ -34,6 +37,14 @@ const appRouter = createBrowserRouter([
       {
         path: "/contact",
         element: <Contact />,
+      },
+      {
+        path: "/grocery",
+        element: (
+          <Suspense fallback={<h1>Loading....</h1>}>
+            <Grocery />
+          </Suspense>
+        ),
       },
       {
         path: "/resturant/:resId",
---
+++ src/components/Grocery.tsx
@@ -0,0 +1,9 @@
+const Grocery = ()=>{
+    return (
+        <div>
+            <h1>This is our Grocery Page, with many child components</h1>
+        </div>
+    )
+};
+
+export default Grocery;
