d3db690d13a0e7ad89c80b2103dca0f016a5c314
Riya Rastogi <riya02rastogi@gmail.com>
2025-09-14 10:33:33 +0530

    Test Cases for rendering contact us page

---
+++ src/components/__tests__/Contact.test.js
@@ -0,0 +1,29 @@
+import { render, screen } from "@testing-library/react"
+import Contact from "../Contact";
+import "@testing-library/jest-dom";
+
+
+test("Should load contact us component",()=>{
+   render(<Contact/>);
+
+   const heading = screen.getByRole("heading");
+   //Assertion
+   expect(heading).toBeInTheDocument();
+});
+
+test("Should load 2 input boxes on contact us component",()=>{
+   render(<Contact/>);
+
+   const input = screen.getAllByRole("textbox");
+   //Assertion
+   expect(input.length).toBe(2);
+});
+
+test("Should load placeholder name on contact us component",()=>{
+   render(<Contact/>);
+
+   const placeholder = screen.getByPlaceholderText("name");
+   //Assertion
+   expect(placeholder).toBeInTheDocument();
+});
+
