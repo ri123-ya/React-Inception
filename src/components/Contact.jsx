b30088a188bc62609f92fc6d3ccf4c846ad8da11
Riya Rastogi <riya02rastogi@gmail.com>
2025-09-14 10:33:47 +0530

    applying css

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,24 @@
 const Contact = () => {
-    return (
-        <div>
-            <h1>Contact Us</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1 className="font-bold text-3xl p-4 m-4">Contact Us</h1>
+      <form>
+        <input
+          type="text"
+          placeholder="name"
+          className="border border-black p-2 m-2"
+        />
+        <input
+          type="text"
+          placeholder="message"
+          className="border border-black p-2 m-2"
+        />
+        <button className="border border-black p-2 m-2 bg-gray-100 rounded-lg">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Contact;
